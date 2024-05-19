af24db65b047f6f8d628975756656df1ae74d7f4
Kumar Saarang Singh <saarang@amazon.com>
2024-05-18 17:46:31 -0700

    Image, Font, Routing, Navigation, Styling

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+import {inter} from "@/app/ui/fonts";
+
+export default function Page() {
+    return <p className={`${inter.className}`}>Invoices page</p>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter, Lusitana} from 'next/font/google'
+
+export const inter = Inter({subsets: ['latin'], weight: '900'})
+export const lusitana = Lusitana({subsets: ['latin'], weight: '700'})
